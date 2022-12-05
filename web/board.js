fbbc7f42b1442dd53686a02c0b0953b5727f3791
paowick <korn2k9@gmail.com>
2022-12-05 22:05:17 +0700

    pieces

--- web/board.js
+++ web/board.js
@@ -1,86 +1,109 @@
-const a8 = document.getElementById("a8")
-const b8 = document.getElementById("b8")
-const c8 = document.getElementById("c8")
-const d8 = document.getElementById("d8")
-const e8 = document.getElementById("e8")
-const f8 = document.getElementById("f8")
-const g8 = document.getElementById("g8")
-const h8 = document.getElementById("h8")
+import {pieces} from './piece.js';
 
-const a7 = document.getElementById("a7")
-const b7 = document.getElementById("b7")
-const c7 = document.getElementById("c7")
-const d7 = document.getElementById("d7")
-const e7 = document.getElementById("e7")
-const f7 = document.getElementById("f7")
-const g7 = document.getElementById("g7")
-const h7 = document.getElementById("h7")
+const a8 = document.querySelector("#a8")
+const b8 = document.querySelector("#b8")
+const c8 = document.querySelector("#c8")
+const d8 = document.querySelector("#d8")
+const e8 = document.querySelector("#e8")
+const f8 = document.querySelector("#f8")
+const g8 = document.querySelector("#g8")
+const h8 = document.querySelector("#h8")
 
-const a6 = document.getElementById("a6")
-const b6 = document.getElementById("b6")
-const c6 = document.getElementById("c6")
-const d6 = document.getElementById("d6")
-const e6 = document.getElementById("e6")
-const f6 = document.getElementById("f6")
-const g6 = document.getElementById("g6")
-const h6 = document.getElementById("h6")
+const a7 = document.querySelector("#a7")
+const b7 = document.querySelector("#b7")
+const c7 = document.querySelector("#c7")
+const d7 = document.querySelector("#d7")
+const e7 = document.querySelector("#e7")
+const f7 = document.querySelector("#f7")
+const g7 = document.querySelector("#g7")
+const h7 = document.querySelector("#h7")
 
-const a5 = document.getElementById("a5")
-const b5 = document.getElementById("b5")
-const c5 = document.getElementById("c5")
-const d5 = document.getElementById("d5")
-const e5 = document.getElementById("e5")
-const f5 = document.getElementById("f5")
-const g5 = document.getElementById("g5")
-const h5 = document.getElementById("h5")
+const a6 = document.querySelector("#a6")
+const b6 = document.querySelector("#b6")
+const c6 = document.querySelector("#c6")
+const d6 = document.querySelector("#d6")
+const e6 = document.querySelector("#e6")
+const f6 = document.querySelector("#f6")
+const g6 = document.querySelector("#g6")
+const h6 = document.querySelector("#h6")
 
-const a4 = document.getElementById("a4")
-const b4 = document.getElementById("b4")
-const c4 = document.getElementById("c4")
-const d4 = document.getElementById("d4")
-const e4 = document.getElementById("e4")
-const f4 = document.getElementById("f4")
-const g4 = document.getElementById("g4")
-const h4 = document.getElementById("h4")
+const a5 = document.querySelector("#a5")
+const b5 = document.querySelector("#b5")
+const c5 = document.querySelector("#c5")
+const d5 = document.querySelector("#d5")
+const e5 = document.querySelector("#e5")
+const f5 = document.querySelector("#f5")
+const g5 = document.querySelector("#g5")
+const h5 = document.querySelector("#h5")
 
-const a3 = document.getElementById("a3")
-const b3 = document.getElementById("b3")
-const c3 = document.getElementById("c3")
-const d3 = document.getElementById("d3")
-const e3 = document.getElementById("e3")
-const f3 = document.getElementById("f3")
-const g3 = document.getElementById("g3")
-const h3 = document.getElementById("h3")
+const a4 = document.querySelector("#a4")
+const b4 = document.querySelector("#b4")
+const c4 = document.querySelector("#c4")
+const d4 = document.querySelector("#d4")
+const e4 = document.querySelector("#e4")
+const f4 = document.querySelector("#f4")
+const g4 = document.querySelector("#g4")
+const h4 = document.querySelector("#h4")
 
-const a2 = document.getElementById("a2")
-const b2 = document.getElementById("b2")
-const c2 = document.getElementById("c2")
-const d2 = document.getElementById("d2")
-const e2 = document.getElementById("e2")
-const f2 = document.getElementById("f2")
-const g2 = document.getElementById("g2")
-const h2 = document.getElementById("h2")
+const a3 = document.querySelector("#a3")
+const b3 = document.querySelector("#b3")
+const c3 = document.querySelector("#c3")
+const d3 = document.querySelector("#d3")
+const e3 = document.querySelector("#e3")
+const f3 = document.querySelector("#f3")
+const g3 = document.querySelector("#g3")
+const h3 = document.querySelector("#h3")
 
-const a1 = document.getElementById("a1")
-const b1 = document.getElementById("b1")
-const c1 = document.getElementById("c1")
-const d1 = document.getElementById("d1")
-const e1 = document.getElementById("e1")
-const f1 = document.getElementById("f1")
-const g1 = document.getElementById("g1")
-const h1 = document.getElementById("h1")
+const a2 = document.querySelector("#a2")
+const b2 = document.querySelector("#b2")
+const c2 = document.querySelector("#c2")
+const d2 = document.querySelector("#d2")
+const e2 = document.querySelector("#e2")
+const f2 = document.querySelector("#f2")
+const g2 = document.querySelector("#g2")
+const h2 = document.querySelector("#h2")
 
+const a1 = document.querySelector("#a1")
+const b1 = document.querySelector("#b1")
+const c1 = document.querySelector("#c1")
+const d1 = document.querySelector("#d1")
+const e1 = document.querySelector("#e1")
+const f1 = document.querySelector("#f1")
+const g1 = document.querySelector("#g1")
+const h1 = document.querySelector("#h1")
+// White_King	  = "&#9812"
+// White_Queen   = "&#9813"
+// White_Rook	  = "&#9814"
+// White_BishopU = "&#9815"
+// White_KnightU = "&#9816"
+// White_Pawn	  = "&#9817"
+// Black_King	  = "&#9818"
+// Black_Queen	  = "&#9819"
+// Black_Rook	  = "&#9820"
+// Black_BishopU = "&#9821"
+// Black_KnightU = "&#9822"
+// Black_Pawn	  = "&#9823"
 
 var board = [
-    [a8,b8,c8,d8,e8,f8,g8,h8],
-    [a7,b7,c7,d7,e7,f7,g7,h7],
-    [a6,b6,c6,d6,e6,f6,g6,h6],
-    [a5,b5,c5,d5,e5,f5,g5,h5],
-    [a4,b4,c4,d4,e4,f4,g4,h4],
-    [a3,b3,c3,d3,e3,f3,g3,h3],
-    [a2,b2,c2,d2,e2,f2,g2,h2],
-    [a1,b1,c1,d1,e1,f1,g1,h1],
+    [[a8,null],[b8,null],[c8,null],[d8,null],[e8,null],[f8,null],[g8,null],[h8,null]],
+    [[a7,null],[b7,null],[c7,null],[d7,null],[e7,null],[f7,null],[g7,null],[h7,null]],
+    [[a6,null],[b6,null],[c6,null],[d6,null],[e6,null],[f6,null],[g6,null],[h6,null]],
+    [[a5,null],[b5,null],[c5,null],[d5,null],[e5,null],[f5,null],[g5,null],[h5,null]],
+    [[a4,null],[b4,null],[c4,null],[d4,null],[e4,null],[f4,null],[g4,null],[h4,null]],
+    [[a3,null],[b3,null],[c3,null],[d3,null],[e3,null],[f3,null],[g3,null],[h3,null]],
+    [[a2,null],[b2,null],[c2,null],[d2,null],[e2,null],[f2,null],[g2,null],[h2,null]],
+    [[a1,null],[b1,null],[c1,null],[d1,null],[e1,null],[f1,null],[g1,null],[h1,null]],
 ] 
+
+var king = new pieces("king",king,"00")
+
+board[0][0][1] = king
+console.log(board[0][0][0])
+console.log(board[0][0][1])
+board[0][0][0].innerHTML = `<div class="boxpiece">&#9812;</div>`;
+
+
+
 
 
 function test(){
--- web/piece.js
+++ web/piece.js
@@ -1,3 +1,16 @@
-class piece{
-     
+export class pieces {
+    constructor(name,ID,pos){
+        this.name = name
+        this.ID = ID
+        this.pos = pos
+    }
+
+    getinfo(){
+        console.log(`${this.name}${this.pos}`)
+    }
+
+
+
 }
+
+
