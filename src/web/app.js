bd6789e980700b921f2d5c180206c7ddea797c48
korrakrit gumnarai <korn2k9@gmail.com>
2023-02-01 14:40:28 +0700

    change dir

---
+++ src/web/app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+const app = express();
+const port = 8080;
+
+import path from "path"
+const __dirname = path.resolve();
+
+app.use(express.static('public'));
+app.use(express.static('public/Game'));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(`${__dirname}/public/Login/login.html`)
+})
+
+app.post('/session', (req,res) => {
+    if(req.body.data != ''){
+        res.redirect('/login')
+        return
+    }
+    // do it after we have login sevice yet
+    res.json(
+        {
+            test:'login'
+        }
+    )
+})
+
+app.get('/Game.html', (req, res) => {
+    res.sendFile(`${__dirname}/public/Game/Game.html`)
+})
+
+app.listen(port, () => {
+    console.log(`listen on port ${port}`);
+})
