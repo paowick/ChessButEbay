60ddb32d3fd804a499f29ecc514d77689526d359
paowick <korn2k9@gmail.com>
2023-02-15 02:02:03 +0700

    fix

--- src/web/public/main/main.js
+++ src/web/public/main/main.js
@@ -1,4 +1,19 @@
-function logout(){
-    browser.cookies.remove()
-    //location.reload()
+//function to clear cookies
+function clearCookies() {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
+// function to handle logout
+function logout() {
+    // clear cookies
+    clearCookies();
+
+    // redirect to login page or home page
+    window.location.href = "/";
 }
