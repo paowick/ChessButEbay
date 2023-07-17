0ab79b646c2bb1988467ff2d247b24d40b00f5e5
paowick <korn2k9@gmail.com>
2023-07-17 23:50:11 +0700

    modified:   src/web/app.js 	new file:   src/web/public/about/about.css 	new file:   src/web/public/about/about.html 	new file:   src/web/public/about/about.js 	renamed:    src/web/public/assets/component/nav.css -> src/web/public/assets/component/navbar/nav.css 	renamed:    src/web/public/assets/component/nav.js -> src/web/public/assets/component/navbar/nav.js 	renamed:    src/web/public/assets/component/navbar.html -> src/web/public/assets/component/navbar/navbar.html 	modified:   src/web/public/main/index.html 	modified:   src/web/public/main/style.css 	modified:   src/web/public/userAuth/forgotpasswordPage/forgot.html 	modified:   src/web/public/userAuth/loginPage/login.html 	modified:   src/web/public/userAuth/signupPage/signup.html 	modified:   src/web/public/userPage/style.css 	modified:   src/web/public/userPage/user.html

--- src/web/public/assets/component/navbar/nav.js
+++ src/web/public/assets/component/navbar/nav.js
@@ -1,30 +1,23 @@
 const path = ["/login", "/signup", "/forgotPassword"]
-
 if (path.includes(window.location.pathname)) {
-
     document.getElementById("login-bar").style.display = "inline-block"
-    document.getElementById("profile").style.display = "none"
+    document.getElementById("pro-con").style.display = "none"
     document.getElementById("manu").style.display = "none"
-
-
 } else {
-
     profile()
     document.getElementById("login-bar").style.display = "none"
 }
-
 function logo() {
-
     window.location.href = "/"
-
 }
-
-
-
 async function profile() {
     const profile = document.getElementById('profile')
     const img = document.createElement("img");
     const user = JSON.parse(localStorage.getItem('user'))
     img.src = `/userimg/getimg?id=${user.id}`
     profile.appendChild(img)
+    const name = document.getElementById("name")
+    const h1_name = document.createElement("h1");
+    h1_name.innerText = user.name
+    name.appendChild(h1_name)
 }
