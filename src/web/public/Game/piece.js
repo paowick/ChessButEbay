290035169101667aba33bc8fad14a2f90bec4e19
paowick <korn2k9@gmail.com>
2023-07-22 14:55:17 +0700

    modified:   src/web/public/Game/piece.js 	modified:   src/web/public/Game/style.css

--- src/web/public/Game/piece.js
+++ src/web/public/Game/piece.js
@@ -127,5 +127,29 @@ export class queen extends pieces {
         var box = document.querySelector(`#${id}`)
         box.innerHTML = `<div class="boxpiece queenWhite">&#9813;</div>`
     }
+
+
+    moveAblepos(board) {
+        const result = []
+        const filterResult = []
+        const row = parseInt(this.pos[1])
+        const col = parseInt(this.pos[0])
+        if (col + 1 < 8) { result.push(`${col + 1}${row}`) };
+        if (col - 1 > -1) { result.push(`${col - 1}${row}`) };
+        if (row + 1 < 8) { result.push(`${col}${row + 1}`) };
+        if (row - 1 > -1) { result.push(`${col}${row - 1}`) };
+        if (col + 1 < 8 && row + 1 < 8) { result.push(`${col + 1}${row + 1}`) };
+        if (col + 1 < 8 && row - 1 > -1) { result.push(`${col + 1}${row - 1}`) };
+        if (col - 1 > -1 && row + 1 < 8) { result.push(`${col - 1}${row + 1}`) };
+        if (col - 1 > -1 && row - 1 > -1) { result.push(`${col - 1}${row - 1}`) };
+
+        for (let index = 0; index < result.length; index++) {
+            const element = result[index];
+            if (board[element[0]][element[1]] != null && board[element[0]][element[1]].team == this.team) {continue}
+            filterResult.push(element)
+        }
+
+        return filterResult
+    }
 }
 
