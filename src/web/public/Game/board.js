c5a6f29d04db9004a349cd1731195b13cc6f8ecb
paowick <korn2k9@gmail.com>
2023-07-23 17:00:11 +0700

    basic chess 80%

--- src/web/public/Game/board.js
+++ src/web/public/Game/board.js
@@ -26,10 +26,42 @@ var board = [
     [null, null, null, null, null, null, null, null],
 ]
 
-new pieces.king("king", "03", "W", true,board)
-new pieces.king("king", "73", "B", true,board)
-// new pieces.queen("queen", "12", "B", false,board)
-// new pieces.queen("queen", "14", "B", false,board)
+new pieces.king("king", "74", "W", true,board)
+new pieces.queen("queen", "73", "W", false,board)
+new pieces.bishop("bishop", "72", "W", false,board)
+new pieces.bishop("bishop", "75", "W", false,board)
+new pieces.rook("rook", "70", "W", false,board)
+new pieces.rook("rook", "77", "W", false,board)
+new pieces.knight("knight", "71", "W", false,board)
+new pieces.knight("knight", "76", "W", false,board)
+
+new pieces.pawn("pawn", "60", "W", false,board,true)
+new pieces.pawn("pawn", "61", "W", false,board,true)
+new pieces.pawn("pawn", "62", "W", false,board,true)
+new pieces.pawn("pawn", "63", "W", false,board,true)
+new pieces.pawn("pawn", "64", "W", false,board,true)
+new pieces.pawn("pawn", "65", "W", false,board,true)
+new pieces.pawn("pawn", "66", "W", false,board,true)
+new pieces.pawn("pawn", "67", "W", false,board,true)
+
+
+new pieces.king("king", "04", "B", true,board)
+new pieces.queen("queen", "03", "B", false,board)
+new pieces.bishop("bishop", "02", "B", false,board)
+new pieces.bishop("bishop", "05", "B", false,board)
+new pieces.rook("rook", "00", "B", false,board)
+new pieces.rook("rook", "07", "B", false,board)
+new pieces.knight("knight", "01", "B", false,board)
+new pieces.knight("knight", "06", "B", false,board)
+
+new pieces.pawn("pawn", "10", "B", false,board,true)
+new pieces.pawn("pawn", "11", "B", false,board,true)
+new pieces.pawn("pawn", "12", "B", false,board,true)
+new pieces.pawn("pawn", "13", "B", false,board,true)
+new pieces.pawn("pawn", "14", "B", false,board,true)
+new pieces.pawn("pawn", "15", "B", false,board,true)
+new pieces.pawn("pawn", "16", "B", false,board,true)
+new pieces.pawn("pawn", "17", "B", false,board,true)
 
 var source = null
 var destination = null
@@ -138,6 +170,7 @@ function moveClient(source, destination) {
     piece.unset()
     piece.pos = newpos
     piece.setPiece()
+    if(piece.firstmove != undefined){piece.firstmove = false}
     // move(source, destination)
 }
 
--- src/web/public/Game/piece.js
+++ src/web/public/Game/piece.js
@@ -85,6 +85,81 @@ export class pieces {
         var box = document.querySelector(`#${id}`)
         box.innerHTML = ""
     }
+
+    recur_top(list, row, col, board) {
+        if (col - 1 > -1) {
+            if (board[col - 1][row] != null && board[col - 1][row].team == this.team) { return }
+            if (board[col - 1][row] != null && board[col - 1][row].team != this.team) { return list.push(`${col - 1}${row}`) }
+            list.push(`${col - 1}${row}`)
+            this.recur_top(list, row, col - 1, board)
+        }
+        return
+    }
+    recur_buttom(list, row, col, board) {
+        if (col + 1 < 8) {
+            if (board[col + 1][row] != null && board[col + 1][row].team == this.team) { return }
+            if (board[col + 1][row] != null && board[col + 1][row].team != this.team) { return list.push(`${col + 1}${row}`) }
+            list.push(`${col + 1}${row}`)
+            this.recur_buttom(list, row, col + 1, board)
+        }
+        return
+    }
+
+    recur_right(list, row, col, board) {
+        if (row + 1 < 8) {
+            if (board[col][row + 1] != null && board[col][row + 1].team == this.team) { return }
+            if (board[col][row + 1] != null && board[col][row + 1].team != this.team) { return list.push(`${col}${row + 1}`) }
+            list.push(`${col}${row + 1}`)
+            this.recur_right(list, row + 1, col, board)
+        }
+        return
+    }
+    recur_left(list, row, col, board) {
+        if (row - 1 > -1) {
+            if (board[col][row - 1] != null && board[col][row - 1].team == this.team) { return }
+            if (board[col][row - 1] != null && board[col][row - 1].team != this.team) { return list.push(`${col}${row - 1}`) }
+            list.push(`${col}${row - 1}`)
+            this.recur_left(list, row - 1, col, board)
+        }
+        return
+    }
+    recur_bottom_right(list, row, col, board) {
+        if (col + 1 < 8 && row + 1 < 8) {
+            if (board[col + 1][row + 1] != null && board[col + 1][row + 1].team == this.team) { return }
+            if (board[col + 1][row + 1] != null && board[col + 1][row + 1].team != this.team) { return list.push(`${col + 1}${row + 1}`) }
+            list.push(`${col + 1}${row + 1}`)
+            this.recur_bottom_right(list, row + 1, col + 1, board)
+        }
+        return
+    }
+    recur_bottom_left(list, row, col, board) {
+        if (col + 1 < 8 && row - 1 > -1) {
+            if (board[col + 1][row - 1] != null && board[col + 1][row - 1].team == this.team) { return }
+            if (board[col + 1][row - 1] != null && board[col + 1][row - 1].team != this.team) { return list.push(`${col + 1}${row - 1}`) }
+            list.push(`${col + 1}${row - 1}`)
+            this.recur_bottom_left(list, row - 1, col + 1, board)
+        }
+        return
+    }
+    recur_top_right(list, row, col, board) {
+        if (col - 1 > -1 && row + 1 < 8) {
+            if (board[col - 1][row + 1] != null && board[col - 1][row + 1].team == this.team) { return }
+            if (board[col - 1][row + 1] != null && board[col - 1][row + 1].team != this.team) { return list.push(`${col - 1}${row + 1}`) }
+            list.push(`${col - 1}${row + 1}`)
+            this.recur_top_right(list, row + 1, col - 1, board)
+        }
+        return
+    }
+    recur_top_left(list, row, col, board) {
+        if (col - 1 > -1 && row - 1 > -1) {
+            if (board[col - 1][row - 1] != null && board[col - 1][row - 1].team == this.team) { return }
+            if (board[col - 1][row - 1] != null && board[col - 1][row - 1].team != this.team) { return list.push(`${col - 1}${row - 1}`) }
+            list.push(`${col - 1}${row - 1}`)
+            this.recur_top_left(list, row - 1, col - 1, board)
+        }
+        return
+    }
+
 }
 
 export class king extends pieces {
@@ -98,27 +173,24 @@ export class king extends pieces {
                 if (element == null || element.team == this.team) { continue }
                 if (element.isKing) { result = result.concat(element.moveAbleposForKing(board)); continue }
                 result = result.concat(element.moveAblepos(board))
-
             }
         }
         return result
     }
-
     setPiece() {
-        if(this.team == "B"){
+        if (this.team == "B") {
             this.board[this.pos[0]][this.pos[1]] = this
             var id = this.tranSlateToId()
             var box = document.querySelector(`#${id}`)
-            box.innerHTML = `<div class="boxpiece kingWhite">&#9818;</div>`
+            box.innerHTML = `<div class="boxpiece kingBlack">&#9818;</div>`
         }
-        if(this.team == "W"){
+        if (this.team == "W") {
             this.board[this.pos[0]][this.pos[1]] = this
             var id = this.tranSlateToId()
             var box = document.querySelector(`#${id}`)
             box.innerHTML = `<div class="boxpiece kingWhite">&#9812;</div>`
         }
     }
-
     moveAblepos(board) {
         const result = []
         const filterResult = []
@@ -132,7 +204,6 @@ export class king extends pieces {
         if (col + 1 < 8 && row - 1 > -1) { result.push(`${col + 1}${row - 1}`) };
         if (col - 1 > -1 && row + 1 < 8) { result.push(`${col - 1}${row + 1}`) };
         if (col - 1 > -1 && row - 1 > -1) { result.push(`${col - 1}${row - 1}`) };
-
         const allEnamyMove = this.findEnamyMove(board)
         for (let index = 0; index < result.length; index++) {
             const element = result[index];
@@ -142,7 +213,6 @@ export class king extends pieces {
             if (allEnamyMove.includes(element)) { continue }
             filterResult.push(element)
         }
-
         return filterResult
     }
 
@@ -159,7 +229,6 @@ export class king extends pieces {
         if (col + 1 < 8 && row - 1 > -1) { result.push(`${col + 1}${row - 1}`) };
         if (col - 1 > -1 && row + 1 < 8) { result.push(`${col - 1}${row + 1}`) };
         if (col - 1 > -1 && row - 1 > -1) { result.push(`${col - 1}${row - 1}`) };
-
         for (let index = 0; index < result.length; index++) {
             const element = result[index];
             const box = board[element[0]][element[1]]
@@ -167,11 +236,8 @@ export class king extends pieces {
                 box.team == this.team) { continue }
             filterResult.push(element)
         }
-
         return filterResult
     }
-
-
 }
 export class queen extends pieces {
 
@@ -180,7 +246,7 @@ export class queen extends pieces {
             this.board[this.pos[0]][this.pos[1]] = this
             var id = this.tranSlateToId()
             var box = document.querySelector(`#${id}`)
-            box.innerHTML = `<div class="boxpiece queenWhite">&#9819;</div>`
+            box.innerHTML = `<div class="boxpiece queenBlack">&#9819;</div>`
         }
         if (this.team == "W") {
             this.board[this.pos[0]][this.pos[1]] = this
@@ -189,29 +255,183 @@ export class queen extends pieces {
             box.innerHTML = `<div class="boxpiece queenWhite">&#9813;</div>`
         }
     }
-
-
     moveAblepos(board) {
         const result = []
         const filterResult = []
         const row = parseInt(this.pos[1])
         const col = parseInt(this.pos[0])
-        if (col + 1 < 8) { result.push(`${col + 1}${row}`) };
-        if (col - 1 > -1) { result.push(`${col - 1}${row}`) };
-        if (row + 1 < 8) { result.push(`${col}${row + 1}`) };
-        if (row - 1 > -1) { result.push(`${col}${row - 1}`) };
-        if (col + 1 < 8 && row + 1 < 8) { result.push(`${col + 1}${row + 1}`) };
-        if (col + 1 < 8 && row - 1 > -1) { result.push(`${col + 1}${row - 1}`) };
-        if (col - 1 > -1 && row + 1 < 8) { result.push(`${col - 1}${row + 1}`) };
-        if (col - 1 > -1 && row - 1 > -1) { result.push(`${col - 1}${row - 1}`) };
-
+        this.recur_top(result, row, col, board)
+        this.recur_buttom(result, row, col, board)
+        this.recur_right(result, row, col, board)
+        this.recur_left(result, row, col, board)
+        this.recur_bottom_right(result, row, col, board)
+        this.recur_bottom_left(result, row, col, board)
+        this.recur_top_right(result, row, col, board)
+        this.recur_top_left(result, row, col, board)
         for (let index = 0; index < result.length; index++) {
             const element = result[index];
             if (board[element[0]][element[1]] != null && board[element[0]][element[1]].team == this.team) { continue }
             filterResult.push(element)
         }
-
         return filterResult
     }
+
 }
 
+
+export class bishop extends pieces {
+
+    setPiece() {
+        if (this.team == "B") {
+            this.board[this.pos[0]][this.pos[1]] = this
+            var id = this.tranSlateToId()
+            var box = document.querySelector(`#${id}`)
+            box.innerHTML = `<div class="boxpiece bishopBlack">&#9821;</div>`
+        }
+        if (this.team == "W") {
+            this.board[this.pos[0]][this.pos[1]] = this
+            var id = this.tranSlateToId()
+            var box = document.querySelector(`#${id}`)
+            box.innerHTML = `<div class="boxpiece bishopWhite">&#9815;</div>`
+        }
+    }
+    moveAblepos(board) {
+        const result = []
+        const filterResult = []
+        const row = parseInt(this.pos[1])
+        const col = parseInt(this.pos[0])
+        this.recur_bottom_right(result, row, col, board)
+        this.recur_bottom_left(result, row, col, board)
+        this.recur_top_right(result, row, col, board)
+        this.recur_top_left(result, row, col, board)
+        for (let index = 0; index < result.length; index++) {
+            const element = result[index];
+            if (board[element[0]][element[1]] != null && board[element[0]][element[1]].team == this.team) { continue }
+            filterResult.push(element)
+        }
+        return filterResult
+    }
+
+}
+
+export class rook extends pieces {
+
+    setPiece() {
+        if (this.team == "B") {
+            this.board[this.pos[0]][this.pos[1]] = this
+            var id = this.tranSlateToId()
+            var box = document.querySelector(`#${id}`)
+            box.innerHTML = `<div class="boxpiece rookBlack">&#9820;</div>`
+        }
+        if (this.team == "W") {
+            this.board[this.pos[0]][this.pos[1]] = this
+            var id = this.tranSlateToId()
+            var box = document.querySelector(`#${id}`)
+            box.innerHTML = `<div class="boxpiece rookWhite">&#9814;</div>`
+        }
+    }
+    moveAblepos(board) {
+        const result = []
+        const filterResult = []
+        const row = parseInt(this.pos[1])
+        const col = parseInt(this.pos[0])
+        this.recur_top(result, row, col, board)
+        this.recur_buttom(result, row, col, board)
+        this.recur_right(result, row, col, board)
+        this.recur_left(result, row, col, board)
+        for (let index = 0; index < result.length; index++) {
+            const element = result[index];
+            if (board[element[0]][element[1]] != null && board[element[0]][element[1]].team == this.team) { continue }
+            filterResult.push(element)
+        }
+        return filterResult
+    }
+
+}
+
+export class knight extends pieces {
+
+    setPiece() {
+        if (this.team == "B") {
+            this.board[this.pos[0]][this.pos[1]] = this
+            var id = this.tranSlateToId()
+            var box = document.querySelector(`#${id}`)
+            box.innerHTML = `<div class="boxpiece rookBlack">&#9822;</div>`
+        }
+        if (this.team == "W") {
+            this.board[this.pos[0]][this.pos[1]] = this
+            var id = this.tranSlateToId()
+            var box = document.querySelector(`#${id}`)
+            box.innerHTML = `<div class="boxpiece rookWhite">&#9816;</div>`
+        }
+    }
+    moveAblepos(board) {
+        const result = []
+        const filterResult = []
+        const row = parseInt(this.pos[1])
+        const col = parseInt(this.pos[0])
+        if (col + 2 < 8 && row + 1 < 8) { result.push(`${col + 2}${row + 1}`) };
+        if (col + 2 < 8 && row - 1 > -1) { result.push(`${col + 2}${row - 1}`) };
+        if (col - 2 > -1 && row + 1 < 8) { result.push(`${col - 2}${row + 1}`) };
+        if (col - 2 > -1 && row - 1 > -1) { result.push(`${col - 2}${row - 1}`) };
+        if (col + 1 < 8 && row + 2 < 8) { result.push(`${col + 1}${row + 2}`) };
+        if (col - 1 > -1 && row + 2 < 8) { result.push(`${col - 1}${row + 2}`) };
+        if (col + 1 < 8 && row - 2 > -1) { result.push(`${col + 1}${row - 2}`) };
+        if (col - 1 > -1 && row - 2 > -1) { result.push(`${col - 1}${row - 2}`) };
+        for (let index = 0; index < result.length; index++) {
+            const element = result[index];
+            if (board[element[0]][element[1]] != null && board[element[0]][element[1]].team == this.team) { continue }
+            filterResult.push(element)
+        }
+        return filterResult
+    }
+
+}
+
+export class pawn extends pieces {
+    constructor(name, pos, team, isKing, board,firstmove) {
+        super(name, pos, team, isKing, board)
+        this.firstmove = firstmove
+    };
+
+    setPiece() {
+        if (this.team == "B") {
+            this.board[this.pos[0]][this.pos[1]] = this
+            var id = this.tranSlateToId()
+            var box = document.querySelector(`#${id}`)
+            box.innerHTML = `<div class="boxpiece rookBlack">&#9823;</div>`
+        }
+        if (this.team == "W") {
+            this.board[this.pos[0]][this.pos[1]] = this
+            var id = this.tranSlateToId()
+            var box = document.querySelector(`#${id}`)
+            box.innerHTML = `<div class="boxpiece rookWhite">&#9817;</div>`
+        }
+    }
+    moveAblepos(board) {
+        const result = []
+        const filterResult = []
+        const row = parseInt(this.pos[1])
+        const col = parseInt(this.pos[0])
+        console.log(this.firstmove);
+        if (this.team == 'B') {
+            if (this.firstmove == true) {
+                result.push(`${col + 1}${row}`)
+                result.push(`${col + 2}${row}`)
+            }else if (col + 1 < 8) { result.push(`${col + 1}${row}`) };
+        }
+        if (this.team == 'W') {
+            if (this.firstmove == true) {
+                result.push(`${col - 1}${row}`)
+                result.push(`${col - 2}${row}`)
+            }else if(col - 1 > -1) { result.push(`${col - 1}${row}`) };
+        }
+        for (let index = 0; index < result.length; index++) {
+            const element = result[index];
+            if (board[element[0]][element[1]] != null && board[element[0]][element[1]].team == this.team) { continue }
+            filterResult.push(element)
+        }
+        return filterResult
+    }
+
+}
